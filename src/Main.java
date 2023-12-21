c0173b95be04019134071565d5802b5ea6a76650
Claudio V. Carta <claudio.vittorio.carta@gmail.com>
2023-12-21 12:10:53 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        media(5,15,10);
+    }
+    public static void media(int a, int b ,int c){
+        int media = (a + b + c) / 3;
+        System.out.println("la media dei 3 numeri é: " + media);
+    }
+}
